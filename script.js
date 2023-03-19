4fec0ad2d4c57dedcc6ceaf3f4e193e7a3313431
TahirRida <tahirrida52@gmail.com>
2023-03-19 20:08:41 +0000

    added some comments

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ function getComputerChoice(){
     let i = Math.trunc(3*Math.random()); //This line generates a random int between 0 and 2 
     return choices[i];
 }
+//Let's create a round function
 function playRound(playerSelection,computerSelection){
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
+    // Now we'll decide the result of the round based on playerSelection value
+    //1- if the player chooses rock
     if (player === "rock"){
         if (computer === "paper") {
             return "You lost!, paper beats rock" ;
@@ -14,6 +17,7 @@ function playRound(playerSelection,computerSelection){
             return "You won! rock beats scissors" ;
         }
     }
+    //2- if the player chooses paper
     if (player === "paper"){
         if (computer === "scissors") {
             return "You lost!, scissors beats paper" ;
@@ -22,6 +26,7 @@ function playRound(playerSelection,computerSelection){
             return "You won! paper beats rock" ;
         }
     } 
+    //3-if the player chooses scissors
     if (player === "scissors"){
         if (computer === "rock") {
             return "You lost!, rock beats scissors" ;
@@ -30,10 +35,12 @@ function playRound(playerSelection,computerSelection){
             return "You won! scissors beats paper" ;
         }
     }
+    //if the player choice is not valid
     else{
         return "please make a valid choice";
     }
 }
+//Let's create a game function knowing that a game is equal to 5 rounds.
 function game(){
     let numberOfPlayerwins = 0;
     let numberOfComputerwins = 0;
